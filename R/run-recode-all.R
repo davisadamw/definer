ce19e65c9229f8970e21ffea2e69bf8ad2471564
Adam Davis <davisadamw@gmail.com>
2020-03-03 15:46:03 -0800

    mans and tests created for def_recode_all

--- R/run-recode-all.R
+++ R/run-recode-all.R
@@ -1,15 +1,29 @@
 #' Recode all columns in a dataset using a definitions list
 #'
-#' @param values_tbl A data frame with values stored in a coded format
-#' @param definitions A named list of named vectors to use for recoding variables.
+#' Column names in \code{values_tbl} should match names in \code{def_list}
 #'
-#' @return A data frame with the same rows and columns as \code{values_tbl}, with columns recoded according to \code{definitions}
+#' @param values_tbl A data frame with values stored in a coded format.
+#' @inheritParams def_recode
+#'
+#' @return A data frame with the same rows and columns as \code{values_tbl}, with columns recoded according to \code{def_recode}
 #' @export
 #'
 #' @examples
-def_recode_all <- function(values_tbl, definitions) {
+#' def_list <- tibble::tibble(variable   = c('A', 'A', 'B', 'B', 'C'),
+#'                            value      = c(1,   2,   1,   2,   1),
+#'                            definition = c('cat', 'dog', 'fred', 'george', 'lonely')) %>%
+#'   def_prep(variable, value, definition, drop_solos = FALSE)
+#'
+#' source_data <- tibble::tibble(ID = c('A1', 'A2', 'A3'),
+#'                               A  = c(1, 1, 2),
+#'                               B  = c(2, 1, 2),
+#'                               C  = c(1, 1, 1))
+#'
+#' def_recode_all(source_data, def_list)
+def_recode_all <- function(values_tbl, def_list, .default = NULL, .missing = NULL) {
   # apply names map ... imap will iterate over columns, giving vector as first arg, col name as second arg
+  # col name will get used to select the appropriate definition set
   values_tbl %>%
-    purrr::imap(def_recode, ) %>%
+    purrr::imap(def_recode_pick, def_list, .default = .default, .missing = .missing) %>%
     tibble::as_tibble()
 }
--- tests/testthat/test-run-recode-all.R
+++ tests/testthat/test-run-recode-all.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("run_recode_all updates all the variables ", {
+  dict_basic <- tibble::tibble(variable   = c('A', 'A', 'B', 'B', 'C'),
+                               value      = c(1,   2,   1,   2,   1),
+                               definition = c('cat', 'dog', 'fred', 'george', 'lonely'))
+
+  def_basic <- def_prep(dict_basic, variable, value, definition, drop_solos = FALSE)
+
+  vals_basic <- tibble::tibble(ID = c('A1', 'A2', 'A3'),
+                               A  = 1:3,
+                               B  = c(1, 1, 2),
+                               C  = c(1, 1, 1))
+
+  vals_recoded <- def_recode_all(vals_basic, def_basic, .default = 'other')
+
+  # just check all the individual columns I guess?
+  expect_equal(vals_recoded$ID, c('A1', 'A2', 'A3'))
+  expect_equal(vals_recoded$A, c('cat', 'dog', 'other'))
+  expect_equal(vals_recoded$B, c('fred', 'fred', 'george'))
+  expect_equal(vals_recoded$C, c('lonely', 'lonely', 'lonely'))
 })
