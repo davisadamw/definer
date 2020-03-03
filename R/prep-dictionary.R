f46ba5d2d7ac2a0a4d2c0aa8636fdf4157468479
Adam Davis <davisadamw@gmail.com>
2020-03-02 18:44:27 -0800

    first function! restructure a data frame containing the value->definition map into a nested list ... failing tests right now and I'm pretty sure this isn't how I want to implement this anyways

---
+++ R/prep-dictionary.R
@@ -0,0 +1,39 @@
+#' Prepare dictionary structure from a data frame with columns identifying variable, value, and definition
+#'
+#' value map will be applied only when starting_value and ending_value are same
+#' to do this, convert it into a nested list
+#' ... list will have elements for columns in pums_hh, each one a named vector of potential values
+#'
+#' @param definitions_tbl A data frame containing information about the definitions of values stored in another table.
+#' @param variable_col Unquoted name of column in `definitions_tbl` that identifies columns in the data table being cleaned
+#' @param value_col Unquoted name of column in `definitions_tbl` that stores current values of variables identified by `variable_col`
+#' @param definition_col Unquoted name of column in `definitions_tbl` that stores replacement / "definition" values matched to `value_col`
+#' @param drop_solos Optionally, logical identifying whether to exclude variables from the dictionary if they only have one unique value
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' tibble(variable   = c('A', 'A', 'B', 'B', 'C'),
+#'            value      = c(1,   2,   1,   2,   1),
+#'            definition = c('cat', 'dog', 'fred', 'george', 'lonely')) %>%
+#'   def_prep(variable, value, definition)
+def_prep <- function(definitions_tbl,
+                     variable_col,
+                     value_col,
+                     definition_col,
+                     drop_solos = TRUE) {
+
+  # first, create a nested
+  definitions_tbl %>%
+    dplyr::select({{ variable_col }},
+                  {{ value_col }},
+                  {{ definition_col }}) %>%
+    dplyr::group_by({{ variable_col }}) %>%
+    tidyr::nest(val_map_tbl = c({{ value_col }}, {{definition_col }})) %>%
+    dplyr::mutate(val_map = purrr::map(val_map_tbl, tibble::deframe)) %>%
+    dplyr::select(-val_map_tbl) %>%
+    tibble::deframe()
+
+}
+
--- tests/testthat/test-prep-dictionary.R
+++ tests/testthat/test-prep-dictionary.R
@@ -1,3 +1,22 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("basic dictionary structure works", {
+  dict_basic <- tibble::tibble(variable   = c('A', 'A', 'B', 'B', 'C'),
+                               value      = c(1,   2,   1,   2,   1),
+                               definition = c('cat', 'dog', 'fred', 'george', 'lonely'))
+
+  # first, test it with
+  expect_equal(def_prep(dict_basic, variable, value, definition),
+               list('A' = c('cat', 'dog'),
+                    'B' = c('fred', 'george')))
+
+})
+
+test_that("variables with incomplete values sets are set up correctly", {
+  dict_basic <- data.frame(variable   = c('A', 'A'),
+                           value      = c(3, 4),
+                           definition = c('bird', 'fish'))
+
+  # I'm not really sure this is desirable behavior here tbh
+  # it might be better to pass in names explicitly instead of creating them
+  expect_equal(def_prep(dict_basic, variable, value, definition),
+               list('A' = c(NA, NA, 'bird', 'fish')))
 })
