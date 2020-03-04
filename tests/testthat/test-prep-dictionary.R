357327380aae83f15e521d72a33fa29fa4b58570
Adam Davis <davisadamw@gmail.com>
2020-03-04 14:14:34 -0800

    single to double quotes in test files

--- tests/testthat/test-prep-dictionary.R
+++ tests/testthat/test-prep-dictionary.R
@@ -1,22 +1,22 @@
 test_that("basic dictionary structure works", {
-  dict_basic <- tibble::tibble(variable   = c('A', 'A', 'B', 'B', 'C'),
+  dict_basic <- tibble::tibble(variable   = c("A", "A", "B", "B", "C"),
                                value      = c(1,   2,   1,   2,   1),
-                               definition = c('cat', 'dog', 'fred', 'george', 'lonely'))
+                               definition = c("cat", "dog", "fred", "george", "lonely"))
 
   def_basic <- def_prep(dict_basic, variable, value, definition)
 
   # first, test it with
   expect_equal(def_basic,
-               list('A' = c(`1` = 'cat', `2` = 'dog'),
-                    'B' = c(`1` = 'fred', `2` = 'george')))
+               list("A" = c(`1` = "cat", `2` = "dog"),
+                    "B" = c(`1` = "fred", `2` = "george")))
 
 })
 
 test_that("variables with incomplete values sets are set up correctly", {
-  dict_basic <- tibble::tibble(variable   = c('A', 'A'),
+  dict_basic <- tibble::tibble(variable   = c("A", "A"),
                                value      = c(3, 4),
-                               definition = c('bird', 'fish'))
+                               definition = c("bird", "fish"))
 
   expect_equal(def_prep(dict_basic, variable, value, definition),
-               list('A' = c(`3` = 'bird', `4` = 'fish')))
+               list("A" = c(`3` = "bird", `4` = "fish")))
 })
--- tests/testthat/test-run-recode.R
+++ tests/testthat/test-run-recode.R
@@ -3,9 +3,9 @@ test_that("recode returns original values when run with null definitions", {
 })
 
 test_that("recode works with definitions produced by def_prep", {
-  dict_basic <- tibble::tibble(variable   = c('A', 'A', 'B', 'B', 'C'),
+  dict_basic <- tibble::tibble(variable   = c("A", "A", "B", "B", "C"),
                                value      = c(1,   2,   1,   2,   1),
-                               definition = c('cat', 'dog', 'fred', 'george', 'lonely'))
+                               definition = c("cat", "dog", "fred", "george", "lonely"))
 
   def_basic <- def_prep(dict_basic, variable, value, definition, drop_solos = FALSE)
 
@@ -15,16 +15,16 @@ test_that("recode works with definitions produced by def_prep", {
 
   # if .x includes values not present in your definitions, it should throw an error, but convert them to NAs if type would change
   expect_warning(recodeA <- def_recode(vals_basic$A, def_basic$A), "Unreplaced values")
-  expect_equal(recodeA, c('cat', 'dog', NA))
-  expect_equal(def_recode(vals_basic$B, def_basic$B), c('fred', 'fred', 'george'))
-  expect_equal(def_recode(vals_basic$C, def_basic$C), rep('lonely', 3))
+  expect_equal(recodeA, c("cat", "dog", NA))
+  expect_equal(def_recode(vals_basic$B, def_basic$B), c("fred", "fred", "george"))
+  expect_equal(def_recode(vals_basic$C, def_basic$C), rep("lonely", 3))
 
 })
 
 test_that("default and missing values are passed through to recode correctly", {
-  dict_basic <- tibble::tibble(variable   = c('A', 'A', 'B', 'B', 'C'),
+  dict_basic <- tibble::tibble(variable   = c("A", "A", "B", "B", "C"),
                                value      = c(1,   2,   1,   2,   1),
-                               definition = c('cat', 'dog', 'fred', 'george', 'lonely'))
+                               definition = c("cat", "dog", "fred", "george", "lonely"))
 
   def_basic <- def_prep(dict_basic, variable, value, definition, drop_solos = FALSE)
 
@@ -32,24 +32,24 @@ test_that("default and missing values are passed through to recode correctly", {
                                B = c(1, 2, NA),
                                C = c(1, 2, NA))
 
-  expect_equal(def_recode(vals_basic$A, def_basic$A, .default = 'fish'), c('cat', 'dog', 'fish'))
-  expect_equal(def_recode(vals_basic$B, def_basic$B, .missing = 'other'), c('fred', 'george', 'other'))
-  expect_equal(def_recode(vals_basic$C, def_basic$C, .default = 'another', .missing = 'other'),
-               c('lonely', 'another', 'other'))
+  expect_equal(def_recode(vals_basic$A, def_basic$A, .default = "fish"), c("cat", "dog", "fish"))
+  expect_equal(def_recode(vals_basic$B, def_basic$B, .missing = "other"), c("fred", "george", "other"))
+  expect_equal(def_recode(vals_basic$C, def_basic$C, .default = "another", .missing = "other"),
+               c("lonely", "another", "other"))
 
 })
 
 test_that("arguments are passed through correctly from def_recode_pick to def_recode", {
-  dict_basic <- tibble::tibble(variable   = c('A', 'A', 'A'),
+  dict_basic <- tibble::tibble(variable   = c("A", "A", "A"),
                                value      = c(1,   2,   3),
-                               definition = c('cat', 'dog', 'fish'))
+                               definition = c("cat", "dog", "fish"))
 
   def_basic <- def_prep(dict_basic, variable, value, definition)
 
-  expect_equal(def_recode_pick(1:3, 'A', def_basic), c('cat', 'dog', 'fish'))
-  expect_equal(def_recode_pick(1:4, 'A', def_basic, .default = 'other'),
-               c('cat', 'dog', 'fish', 'other'))
-  expect_equal(def_recode_pick(c(1:3, NA), 'A', def_basic, .missing = 'other2'),
-               c('cat', 'dog', 'fish', 'other2'))
-  expect_equal(def_recode_pick(1:3, 'Z', def_basic), 1:3)
+  expect_equal(def_recode_pick(1:3, "A", def_basic), c("cat", "dog", "fish"))
+  expect_equal(def_recode_pick(1:4, "A", def_basic, .default = "other"),
+               c("cat", "dog", "fish", "other"))
+  expect_equal(def_recode_pick(c(1:3, NA), "A", def_basic, .missing = "other2"),
+               c("cat", "dog", "fish", "other2"))
+  expect_equal(def_recode_pick(1:3, "Z", def_basic), 1:3)
 })
