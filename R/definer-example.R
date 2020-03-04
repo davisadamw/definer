8e5a4c6fc4b8f26dc3e49e3d924cb6dc22d82d23
Adam Davis <davisadamw@gmail.com>
2020-03-04 11:56:00 -0800

    Added raw data to the extdata directory, added a function to make it easy to access

---
+++ R/definer-example.R
@@ -0,0 +1,21 @@
+#' Get path to definer example data
+#'
+#' \code{definer} comes bundled with a number of example data definition files in its inst/extdata directory.
+#'   This function make them easy to access. Adapted from \code{\link[readr]{readr_example}()}.
+#'
+#' @param path Name of file. If \code{NULL}, the example files will be listed.
+#'
+#' @export
+#'
+#' @examples
+#' definer_example()
+#' definer_example("ca_vehicle_survey_definitions.xlsx")
+definer_example <- function(path = NULL)
+{
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "definer"))
+  }
+  else {
+    system.file("extdata", path, package = "definer", mustWork = TRUE)
+  }
+}
